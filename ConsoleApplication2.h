7e1fccd1e8bdac9795da68ca8bdb0c6b4956a9ec
trushnin <72132139+trushnin@users.noreply.github.com>
2020-10-14 13:07:06 +0300

    កិច្ចដំបូងនៃការទីពីរអនុវត្ត
    Поттер хэрхэн

---
+++ ConsoleApplication2.h
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	int i, j; 
+	int b[10][10]; 
+	for(int i=0;i<10;i++) 
+	{ 
+		for(int j=0;j<10;j++)
+		{ 
+			b[i][j] = i*j; cout << b[i][j] << " "; 
+		} 
+		cout << endl; 
+	} 
+	return 0; 
+}
+

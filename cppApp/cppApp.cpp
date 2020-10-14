3a39a12c1e1494552d053ccd1f04abc9bf1052fb
fatyhramil <Ramilfatykhov@mail.ru>
2020-10-14 23:27:52 +0300

    ver. 0.1

--- cppApp/cppApp.cpp
+++ cppApp/cppApp.cpp
@@ -1,26 +1,14 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using std::cin;
-using std::cout;
-using std::endl;
 
+#include <iostream>
+#include "ArrayGeneration.h"
+using namespace std;
 int main()
 {
-     int numbers[5] = {1, 2, 3, 4, 5};
-    int twoDimN[5][5];
-    srand(static_cast<int>(time(0)));
-    for (int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
-    {
-        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++)
-        {
-            twoDimN[i][j] = rand();
-            cout << twoDimN[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-
- 
+	ArrayGeneration ag;
+	ag.OneDimentionArrayGen();
+	ag.OneDimentionArray();
+	ag.TwoDimensionArrayGen();
+	ag.TwoDimensionArray();
+	ag.OneDimensionSort();
 }
- 
+

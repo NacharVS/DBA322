c6d4e2dbbfa6c9e198a13749b8f07dfd587239e4
r3dinc <r3dinc@yandex.ru>
2020-10-17 08:46:14 +0300

    fix

--- cppApp/cppApp.cpp
+++ cppApp/cppApp.cpp
@@ -7,20 +7,5 @@ using std::endl;
 
 int main()
 {
-     int numbers[5] = {1, 2, 3, 4, 5};
-    int twoDimN[5][5];
-    srand(static_cast<int>(time(0)));
-    for (int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
-    {
-        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++)
-        {
-            twoDimN[i][j] = rand();
-            cout << twoDimN[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-
- 
 }
  

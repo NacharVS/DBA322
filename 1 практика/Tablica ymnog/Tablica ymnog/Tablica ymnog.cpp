e89510e8c2a43fc3490fa79a71ac55d298aabfa4
Youngsteeeer <72131923+Youngsteeeer@users.noreply.github.com>
2020-10-17 09:04:21 +0300

    Add files via upload

---
+++ 1 практика/Tablica ymnog/Tablica ymnog/Tablica ymnog.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+   
+    const int size = 10;
+    int i = 0, j = 0;
+
+    for (i = 1; i <= size; i++)
+    {
+        cout << endl
+            << setw(7) << i << "   |";
+        for (j = 1; j <= size; j++)
+            cout << setw(3) << i * j << " ";
+    }
+
+    cin.ignore();
+    cin.get();
+    return 0;
+}
+
---
+++ 1 практика/a в степени b/a в степени b/a в степени b.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b, w=1;
+    std::cout << "Vvedite chislo: \n";
+    std::cin >> b;
+    std::cout << "Vvedite stepen: \n";
+    std::cin >> a;
+    int q = b;
+    if (a == 0) { std:: cout << w; return 0; }
+    for (int i = 1; i < a; i++)
+    {
+        b *= q;
+    }
+    std::cout << "Otvet: \n"; 
+    std::cout << b;
+}
+

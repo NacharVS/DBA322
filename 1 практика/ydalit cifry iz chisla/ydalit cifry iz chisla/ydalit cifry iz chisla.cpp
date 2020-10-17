d3ba92e15368f5af1413fadd448b45f8c06d79f0
Alexey-Plyushch <72132503+Alexey-Plyushch@users.noreply.github.com>
2020-10-17 08:56:41 +0300

    Add files via upload

---
+++ 1 практика/ydalit cifry iz chisla/ydalit cifry iz chisla/ydalit cifry iz chisla.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    int number, digit;
+    string str;
+    stringstream s;
+    cout << "Vvedite chislo: \n";
+    cin >> number;
+    cout << "Ydalite chislo: \n";
+    cin >> digit;
+    s << number;
+    str = s.str();
+    s.str("");
+    str.erase(str.find(digit + '0', 0), 1);
+    s << str;
+    s >> number;
+    cout << "Otvet: ";
+    cout << number;
+
+    system("pause");
+    return 0;
+}

b2e0d9552fce24853075160017668d59252d85c8
musahamidullin <73012632+musahamidullin@users.noreply.github.com>
2020-10-20 22:58:22 +0300

    practics
    practics

---
+++ c++ вторая практика.cpp
@@ -0,0 +1,124 @@
+﻿
+#include <iostream>
+#include <ctime>
+#include <array>
+using namespace std;
+
+void diagonalSum(int array_array[50][50], int n_n)
+{
+	int diag_sum = 0;
+	for (int i = 0; i < n_n; ++i)
+	{
+		for (int j = 0; j < n_n; ++j)
+		{
+			if (i == j)
+			{
+				diag_sum += array_array[i][j];
+			}
+		}
+	}
+	cout << "summ diagonal: " << diag_sum;
+}
+
+void GetRandom(int array[50],int n)
+{
+	
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		array[i] = rand();
+		cout << array[i] << " ";
+	}
+	
+}
+
+void BubbleSorted(int array[50],int n)
+{
+	int temp;
+	for (int i = 1; i < n; ++i)
+	{
+		for (int j = 0; j < (n - i); ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			
+		}
+	}
+	for (int j = 0; j < n; ++j)
+	{
+		cout << array[j] << " ";
+	}
+}
+
+void GetRandomMultidemensional(int array_array[50][50], int n_n)
+{
+	for (int i = 0; i < n_n; i++)
+	{
+		for (int j = 0; j < n_n; j++)
+		{
+			array_array[i][j] = rand();
+			cout << array_array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+}
+void MaxIndexMultiedemnsional(int array_array[50][50], int n_n)
+{
+	int max;
+	max = array_array[0][0];
+	for (int i = 0; i < n_n; i++)
+	{
+		for (int j = 0; j < n_n; j++)
+		{
+			if (array_array[i][j] > max)
+			{
+				max = array_array[i][j];
+				int str = i + 1;
+				int stl = j + 1;
+				cout << "Max: " << max << "located in " << str << stl;
+			}
+		}
+	}
+	/*for (int i = 0; i < n_n; i++)
+	{
+		for (int j = 0; j < n_n; j++)
+		{
+			cout << array_array[i][j];
+		}
+	}*/
+}
+ 
+int main()
+{ // 1 - задание
+	/*cout << "Enter the size of array: " << endl << endl;
+	int array[50];	
+	int n;
+	cin >> n;
+	cout << endl;
+	GetRandom(array, n);
+
+	cout << endl << endl;
+	cout << "sorted:" << endl << endl;
+	
+	BubbleSorted(array,n);*/
+
+// 2 - задание
+	cout << "Enter the size of multidemensional array: " << endl << endl;
+	int array_array[50][50];
+	int n_n;
+	cin >> n_n;
+	cout << endl;
+	GetRandomMultidemensional(array_array,n_n);
+	//cout << endl << endl << "sorted" << endl << endl;
+	//MaxIndexMultiedemnsional(array_array, n_n);
+
+// 3 - задание 
+	cout << endl << endl;
+	diagonalSum(array_array, n_n);
+
+}
---
+++ third_practic.cpp
@@ -0,0 +1,80 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+enum Elements
+{
+	i,
+	u,
+	r
+};
+
+int main()
+{
+	//double a, b, c;
+	//cout << "input a: " << endl;
+	//cin >> a;
+	//cout << "input b: " << endl;
+	//cin >> b;
+	//cout << "input c: " << endl;
+	//cin >> c;
+
+	//UravnenieCalc(a, b, c);
+
+
+	cout << endl << endl << "switch physical element: ";
+	SwitchPhysicalElement(i);
+}
+
+//void UravnenieCalc(double a, double b, double c)
+//{
+//	if ((b * b - 4 * a * c) >= 0)
+//	{
+//		double x;
+//		x = ((b * -1) + sqrt(b * b - 4 * a * c)) / ((2 * a));
+//		cout << "x1: " << x << endl << endl;
+//		x = ((b * -1) - sqrt(b * b - 4 * a * c)) / ((2 * a));
+//		cout << "x2: " << x;
+//	}
+//	else
+//	{
+//		cout << "error";
+//	}
+//}
+
+void SwitchPhysicalElement(Elements element)
+{
+	switch (element)
+	{
+	case i:
+		int I, U, R;
+		cout << "input U";
+		cin >> U;
+		cout << "input R";
+		cin >> R;
+		I = U / R;
+		cout << "i = u / r" << endl << I;
+
+	case u:
+		int I, U, R;
+		cout << "input I";
+		cin >> I;
+		cout << "input R";
+		cin >> R;
+		U = I * R;
+		cout << "u = i * r" << endl << U;
+	case r:
+		int I, U, R;
+		cout << "input U";
+		cin >> U;
+		cout << "input I";
+		cin >> I;
+		R = U / I;
+		cout << "r = u / i" << endl << R;
+	default:
+		break;
+	}
+}
+
+

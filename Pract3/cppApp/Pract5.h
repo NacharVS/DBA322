435d55fe89f1f4f1748a909277b5eebdc7b36488
fatyhramil <Ramilfatykhov@mail.ru>
2020-10-21 22:33:50 +0300

    ver 0.41

--- Pract3/cppApp/Pract5.h
+++ Pract3/cppApp/Pract5.h
@@ -16,21 +16,49 @@ public:
 			return mas[i] + SummaElementov(mas, i-1);
 		}
 	}
+//public:
+//	static int ColvoSymbol(string str,int i, char ch)
+//	{
+//		double x = str.length();
+//		if (i == x)
+//		{
+//			return 0;
+//		}
+//		else
+//		{
+//			i++;
+//			if (str[i] == ch)
+//			{
+//				return 1 + ColvoSymbol(str, i, ch);
+//			}
+//		}
+//	}
+//public: static short ColvoSymbol(string str, char c)
+//{
+//	int i = 0;
+//	i = str.find(c, i);
+//	if (i > str.length()) 
+//	{ 
+//		return 0; 
+//	}
+//	else
+//	{
+//		int count = 0;
+//		count++;
+//		i++;
+//		return count + ColvoSymbol(str, c);
+//	}
+//	
+//
+//}
 public:
-	static int ColvoSymbol(string str, int i)
+	static size_t ColvoSymbol(const string& s, char c)
 	{
-		double x = str.length();
-		if (i == x)
-		{
-			return 0;
-		}
-		else
-		{
-			i++;
-			return 1 + ColvoSymbol(str,i);
-		}
+		string::size_type pos = s.find(c);
+		return (pos == string::npos) ? 0 : (1 + ColvoSymbol(s.substr(pos + 1), c));
 	}
-public: static int MaxDivided(int n, int m)
+public:
+	static int MaxDivided(int n, int m)
 {
 	if (n == m)
 	{
--- Pract3/cppApp/cppApp.cpp
+++ Pract3/cppApp/cppApp.cpp
@@ -9,7 +9,9 @@ int main()
 	Pract5 pr;
 	double mas[]{ 4,32, 64 };
 	cout << pr.SummaElementov(mas,2)<<endl;
-	cout << pr.ColvoSymbol("Ramil", 0)<<endl;
+	char a;
+	cin >> a;
+	cout << pr.ColvoSymbol("Ramil Fatykhov", a)<<endl;
 	cout << pr.MaxDivided(12,6);
 }
 

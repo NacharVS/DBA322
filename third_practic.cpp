3f31c9e1270c07354312fdbf7d9784d97b052c5c
musahamidullin <73012632+musahamidullin@users.noreply.github.com>
2020-10-23 23:17:23 +0300

    Законченная 3 практика
    Законченная 3 практика

--- third_practic.cpp
+++ third_practic.cpp
@@ -10,23 +10,6 @@ enum Elements
 	r
 };
 
-int main()
-{
-	//double a, b, c;
-	//cout << "input a: " << endl;
-	//cin >> a;
-	//cout << "input b: " << endl;
-	//cin >> b;
-	//cout << "input c: " << endl;
-	//cin >> c;
-
-	//UravnenieCalc(a, b, c);
-
-
-	cout << endl << endl << "switch physical element: ";
-	SwitchPhysicalElement(i);
-}
-
 //void UravnenieCalc(double a, double b, double c)
 //{
 //	if ((b * b - 4 * a * c) >= 0)
@@ -44,37 +27,75 @@ int main()
 //}
 
 void SwitchPhysicalElement(Elements element)
-{
+{		int I, U, R;
 	switch (element)
 	{
 	case i:
-		int I, U, R;
+		
 		cout << "input U";
-		cin >> U;
+		std::cin >> U;
 		cout << "input R";
-		cin >> R;
+		std::cin >> R;
 		I = U / R;
 		cout << "i = u / r" << endl << I;
 
 	case u:
-		int I, U, R;
+		
 		cout << "input I";
-		cin >> I;
+		std::cin >> I;
 		cout << "input R";
-		cin >> R;
+		std::cin >> R;
 		U = I * R;
 		cout << "u = i * r" << endl << U;
 	case r:
-		int I, U, R;
+	
 		cout << "input U";
-		cin >> U;
+		std::cin >> U;
 		cout << "input I";
-		cin >> I;
+		std::cin >> I;
 		R = U / I;
 		cout << "r = u / i" << endl << R;
 	default:
 		break;
+	};
+}
+
+void IntervalCalc(int n, int x, int y)
+{
+	if (n < x && n < y)
+	{
+		cout << "yes";
 	}
+	else
+	{
+		cout << "no";
+	}
+}
+
+int main()
+{
+
+	//double a, b, c;
+	//cout << "input a: " << endl;
+	//cin >> a;
+	//cout << "input b: " << endl;
+	//cin >> b;
+	//cout << "input c: " << endl;
+	//cin >> c;
+
+	//UravnenieCalc(a, b, c);
+
+
+	//cout << endl << endl << "switch physical element: ";
+	//SwitchPhysicalElement(i);
+
+
+	int n, k, x, y, a;
+	cout << "number and input x,y" << endl;
+	cin >> n;
+	cin >> x >> y;
+	IntervalCalc(n, x, y);
+	
 }
 
 

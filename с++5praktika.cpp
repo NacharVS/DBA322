32fe33c262d9e431f2f3b6c5f45f6c0cd8a38314
musahamidullin <73012632+musahamidullin@users.noreply.github.com>
2020-10-24 09:08:24 +0300

    работа в на паре
    работа в на паре

---
+++ с++5praktika.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=10, b=15, c;
+	int* rasstoyanie_A = &a, * rasstoyanie_B = &b;
+	cout << "a=" << a << "\t&a = " << &a << "\t*rasstoyanie_A = " << *rasstoyanie_A << endl;
+	cout << "b=" << b << "\t&b = " << &b << "\t*rasstoyanie_B = " << *rasstoyanie_B << endl;
+	cout << "&a: " << &a << "\n&b: " << &b << "\n&a - &b: " << ((int*)&a - (int*)&b) << endl;
+}
+ 

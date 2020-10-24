ecb41ccb1f6b6746c166bb783dfb4ba23afa9986
fatyhramil <Ramilfatykhov@mail.ru>
2020-10-24 22:10:54 +0300

    ver 0.42

--- Pract3/cppApp/cppApp.cpp
+++ Pract3/cppApp/cppApp.cpp
@@ -3,15 +3,20 @@
 #include "ArrayGeneration.h"
 #include "Pract5.h"
 #include <string>
+#include <cmath>
 using namespace std;
 int main()
 {
-	Pract5 pr;
-	double mas[]{ 4,32, 64 };
-	cout << pr.SummaElementov(mas,2)<<endl;
-	char a;
-	cin >> a;
-	cout << pr.ColvoSymbol("Ramil Fatykhov", a)<<endl;
-	cout << pr.MaxDivided(12,6);
+	int n = 5;
+	string g = "Freddie Mercury";
+	string h = "David Bowie";
+	int j = 1488;
+	double m = 10;
+	int* x1 = &n;
+	double* x2 = &m;
+	int len = abs(((int)x1 - (int)x2) / 8);
+	cout << (int)x1 << endl;
+	cout << (int)x2 << endl;
+	cout << len;
 }
 
